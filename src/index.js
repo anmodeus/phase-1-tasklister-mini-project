69fb3da10d6e65bf7ed33f653f0f89985b836727
Anna Perez <anna.perez.guitar@gmail.com>
2023-05-02 21:12:16 -0700

    displays task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  //let form = document.querySelector('form')
+  addingEventListeners()
+})
+
+function addingEventListeners() {
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+  const task = e.target["new-task-description"].value
+  
+  displayTask(task)
+}
+
+function displayTask(task){
+  const taskUl = document.getElementById("tasks")
+
+  const taskLi = document.createElement("li")
+
+  taskLi.textContent = task
+  taskUl.appendChild(taskLi)
+}
